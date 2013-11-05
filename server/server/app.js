69a96810d212d47e028c51f76481ecd24afafec8
sekka1 <garlandk@gmail.com>
2013-11-04 19:22:29 -0800

    added server and initial auth mechanism

---
+++ server/server/app.js
@@ -0,0 +1,137 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var hash = require('./pass').hash;
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 8080);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+
+
+
+
+/////////////////////////
+/////////////////////////
+
+// Auth
+// https://github.com/visionmedia/express/blob/master/examples/auth/app.js
+
+// dummy database
+var users = {
+    tj: { name: 'tj' }
+};
+
+// when you create a user, generate a salt
+// and hash the password ('foobar' is the pass here)
+hash('foobar', function(err, salt, hash){
+    if (err) throw err;
+    // store the salt & hash in the "db"
+    users.tj.salt = salt;
+    users.tj.hash = hash;
+});
+
+
+// Authenticate using our plain-object database of doom!
+
+function authenticate(name, pass, fn) {
+
+    //if (!module.parent) console.log('authenticating %s:%s', name, pass);
+    var user = users[name];
+
+    // query the db for the given username
+    if (!user) return fn(new Error('cannot find user'));
+
+    // apply the same algorithm to the POSTed password, applying
+    // the hash against the pass / salt, if there is a match we
+    // found the user
+    hash(pass, user.salt, function(err, hash){
+
+        if (err) return fn(err);
+        if (hash == user.hash) return fn(null, user);
+        fn(new Error('invalid password'));
+    })
+
+}
+
+function restrict(req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        req.session.error = 'Access denied!';
+        res.send('access denied');
+    }
+}
+
+
+app.get('/restricted', restrict, function(req, res){
+    res.send('Wahoo! restricted area, click to <a href="/logout">logout</a>');
+});
+
+app.post('/login', function(req, res){
+    authenticate(req.body.username, req.body.password, function(err, user){
+        if (user) {
+            // Regenerate session when signing in
+            // to prevent fixation
+            req.session.regenerate(function(){
+                // Store the user's primary key
+                // in the session store to be retrieved,
+                // or in this case the entire user object
+                req.session.user = user;
+                req.session.success = 'Authenticated as ' + user.name
+                    + ' click to <a href="/logout">logout</a>. '
+                    + ' You may now access <a href="/restricted">/restricted</a>.';
+                //res.redirect('back');
+                res.send('authentication success');
+            });
+        } else {
+            req.session.error = 'Authentication failed, please check your '
+                + ' username and password.'
+                + ' (use "tj" and "foobar")';
+            res.send('authentication failed, please check your username and password');
+        }
+    });
+});
+
+app.get('/logout', function(req, res){
+    // destroy the user's session to log them out
+    // will be re-created next request
+    req.session.destroy(function(){
+        res.send('logged out');
+    });
+});
+
+
+/////////////////////////
+/////////////////////////
+
+
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+app.get('/users', restrict, user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
